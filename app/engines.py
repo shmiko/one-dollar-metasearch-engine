78e45b305b141ec0d3a7ead4160249a14aa9aa4a
Keshi Dai <keshi.dai@intentmedia.com>
2013-02-17 15:59:07 -0500

    KD: Use requsts to replacde urllib

--- app/engines.py
+++ app/engines.py
@@ -1,19 +1,26 @@
-import urllib
+from apiclient.errors import HttpError
+from httplib2 import HttpLib2Error
+import requests
 from apiclient.discovery import build
 from bs4 import BeautifulSoup
 
 def get_google_results(query):
-    google = build("customsearch", "v1", developerKey="AIzaSyA4bQ5NdaPpoJBVuC-cZatqCCdTvc570QE")
-    res = google.cse().list(q=query, cx='017576662512468239146:omuauf_lfve').execute()
-    return res['items']
+    try:
+        google = build("customsearch", "v1", developerKey="AIzaSyA4bQ5NdaPpoJBVuC-cZatqCCdTvc570QE")
+        res = google.cse().list(q=query, cx='017576662512468239146:omuauf_lfve').execute()
+        return res['items']
+    except (HttpError, HttpLib2Error):
+        return []
 
 def get_bing_results(query):
     url = "http://www.bing.com/search?q=%s" % query
-    doc = urllib.urlopen(url).read()
-    results = BeautifulSoup(doc).find("div", {"id": "results"}).findAll('li', {'class': 'sa_wr'})
+    res = requests.get(url)
+    if res.status_code == requests.codes.ok:
+        results = BeautifulSoup(res.text).find("div", {"id": "results"}).findAll('li', {'class': 'sa_wr'})
 
-    res = [{'link': result.find('div', {'class': 'sb_meta'}).find('cite').getText(),
-            'title': result.find('div', {'class': 'sb_tlst'}).getText(),
-            'snippet': result.find('p').getText()}
-           for result in results]
-    return res
+        doc = [{'link': result.find('div', {'class': 'sb_meta'}).find('cite').getText(),
+                'title': result.find('div', {'class': 'sb_tlst'}).getText(),
+                'snippet': result.find('p').getText()}
+               for result in results]
+        return doc
+    return []
