1cd78b5490e8223f98ef82b7f44c28220ca2d28d
Keshi Dai <keshi.dai@intentmedia.com>
2013-02-18 12:32:43 -0500

    KD: Add yahoo search results, and use lxml to parse the search results

--- app/engines.py
+++ app/engines.py
@@ -9,18 +9,37 @@ def get_google_results(query):
         google = build("customsearch", "v1", developerKey="AIzaSyA4bQ5NdaPpoJBVuC-cZatqCCdTvc570QE")
         res = google.cse().list(q=query, cx='017576662512468239146:omuauf_lfve').execute()
         return res['items']
-    except (HttpError, HttpLib2Error):
+    except (HttpError, HttpLib2Error, Exception):
         return []
 
 def get_bing_results(query):
     url = "http://www.bing.com/search?q=%s" % query
     res = requests.get(url)
     if res.status_code == requests.codes.ok:
-        results = BeautifulSoup(res.text).find("div", {"id": "results"}).findAll('li', {'class': 'sa_wr'})
+        results = BeautifulSoup(res.text, 'lxml').find("div", {"id": "results"}).find_all('li', {'class': 'sa_wr'})
 
-        doc = [{'link': result.find('div', {'class': 'sb_meta'}).find('cite').getText(),
-                'title': result.find('div', {'class': 'sb_tlst'}).getText(),
-                'snippet': result.find('p').getText()}
-               for result in results]
-        return doc
+        docs = []
+        for result in results:
+            title = result.find('div', {'class': 'sb_tlst'}).getText()
+            link = result.find('div', {'class': 'sb_meta'}).find('cite').getText()
+            snippet_tag = result.find('p')
+            snippet = '' if snippet_tag is None else snippet_tag.getText()
+            docs.append({'link': link, 'title': title, 'snippet': snippet})
+        return docs
+    return []
+
+def get_yahoo_results(query):
+    url = "http://search.yahoo.com/search?p=%s" % query
+    res = requests.get(url)
+    if res.status_code == requests.codes.ok:
+        results = BeautifulSoup(res.text, 'lxml').find_all('div', {'class': 'res'})
+
+        docs = []
+        for result in results:
+            title = result.find('h3').getText()
+            link = result.find('h3').find('a', href=True)['href']
+            snippet_tag = result.find('div', {'class':'abstr'})
+            snippet = '' if snippet_tag is None else snippet_tag.getText()
+            docs.append({'link': link, 'title': title, 'snippet': snippet})
+        return docs
     return []
--- app/search.py
+++ app/search.py
@@ -2,13 +2,14 @@ import re
 import gevent
 from flask import render_template, request
 from app import app
-from app.engines import get_google_results, get_bing_results
+from app.engines import get_google_results, get_bing_results, get_yahoo_results
 
 @app.route('/')
 def hello():
     return render_template('search.html')
 
-@app.route('/search', methods=['POST'])
+
+@app.route('/search', methods = ['POST'])
 def search():
     query = request.form['query']
     if len(query.strip()) > 0:
@@ -20,16 +21,20 @@ def search():
 def _fetch_results(query):
     query = _format_query(query)
     #TODO: use twisted or tornado
-    jobs = [gevent.spawn(get_google_results, query), gevent.spawn(get_bing_results, query)]
+    jobs = [gevent.spawn(get_google_results, query),
+            gevent.spawn(get_bing_results, query),
+            gevent.spawn(get_yahoo_results, query)]
     gevent.joinall(jobs)
     results = [job.get() for job in jobs]
     return results
+
 
 def _format_query(query):
     query = re.sub(r'[^\w\s]', ' ', query).lower()
     tokens = re.split(r'\s+', query)
     tokens = [token.strip() for token in tokens]
     return '+'.join(tokens)
+
 
 def _merge(candidates):
     retrieved_docs = candidates[0]
